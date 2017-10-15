ddf007ffe3b270aa9cf4e846303a1acd5beee583
Taiju Aoki <aokitaiju0513@gmail.com>
2017-10-16 03:13:45 +0900

    add gitignore and clean up a bit

--- init.sh
+++ init.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-mkdir -p "log";
-touch "log/log.txt";
+YOUR_LOG_DIRECTORY="log"
+YOUR_LOG_FILE="log.txt"
 
+mkdir -p ${YOUR_LOG_DIRECTORY};
+touch ${YOUR_LOG_DIRECTORY}/${YOUR_LOG_FILE};
+
+echo 0 >> ${YOUR_LOG_DIRECTORY}/${YOUR_LOG_FILE}
 echo "create your log directory and logfile!"
--- monitor.py
+++ monitor.py
@@ -19,35 +19,28 @@ your_thesis_type = 'YOUR_THESIS_TYPE'
 t = Twitter(auth = OAuth(access_token, access_token_secret, api_key, api_secret))
 
 # read saved progress
-file = open(logFilePath, 'r')
+file = open(log_file_path, 'r')
 old_page = file.read()
-print('old_page:')
-print(old_page)
 
 # get current page number
-number_of_pages = cg.CGPDFDocumentCreateWithProvider(cg.CGDataProviderCreateWithFilename(pdfFilePath)).getNumberOfPages()
-print('current_page:')
-print(number_of_pages)
+number_of_pages = cg.CGPDFDocumentCreateWithProvider(cg.CGDataProviderCreateWithFilename(pdf_file_path)).getNumberOfPages()
 
 # calc delta page
 delta_page = number_of_pages - int(old_page)
-print('delta_page:')
-print(delta_page)
 
 # make text to post
 text0 = your_name + 'さんの\n'
 text1 = your_thesis_type + '論文の総ページ数は，' + str(number_of_pages) + 'ページです！\n'
 text2 = '昨日からの進捗は' + str(delta_page) + 'ページです'
 text = text0 + text1 + text2
-print(text)
 
 # post tweet
 statusUpdate = t.statuses.update(status=text)
 
-# data
-print(statusUpdate['user']['screen_name'])
-print(statusUpdate['user']['name'])
-print(statusUpdate['text'])
+# posted text and user data
+# print(statusUpdate['user']['screen_name'])
+# print(statusUpdate['user']['name'])
+# print(statusUpdate['text'])
 
 # save progress
 file2 = open(log_file_path, 'w')
