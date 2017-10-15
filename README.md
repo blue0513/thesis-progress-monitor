7e7f3a47fbf2060ff76ef641b4eaceefd6df1161
Taiju Aoki <aokitaiju0513@gmail.com>
2017-10-16 02:34:00 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# Thesis Progress Monitor
+
+Create a bot which can monitor the progress of your paper
+
+## USAGE
+
+### Get Your Twitter API
+
+1. You need to get a twitter api access key
+   1. Open https://apps.twitter.com/ 
+   2. Click **Create New App**
+   3. Fill out info (Note: Website URL should be filled out. I recommend `127.0.0.1`)
+   4. Click **Create your Twitter application**
+   5. Go to the next step!
+
+2. Set those access keys in `monitor.py`
+```python
+access_token = "YOUR_ACCESS_TOKEN"
+access_token_secret = "YOUR_SECRET_ACCESS_TOKEN"
+api_key = "YOUR_API_KEY"
+api_secret = "YOUR_SECRET_API_KEY"
+```
+
+### Post Your Progress on Twitter
+
+You need to make a directory and file to save **your progress log**
+```shell
+$ cd thesis-progress-monitor
+$ ./init.sh
+```
+
+Set your parameters in `monitor.py`
+```python
+log_file_path = 'YOUR_LOG_PATH'
+pdf_file_path = 'YOUR_PDF_PATH'
+your_name = 'YOUR_NAME'
+your_thesis_type = 'YOUR_THESIS_TYPE'
+```
+
+Post your progress on twitter!!
+```shell
+$ python monitor.py
+```
+
+### Monitor Your Progress
+Turn on `crontab` and monitor your progress
+
+1. On the terminal, run the command 
+```shell
+$ crontab -e
+```
+
+2. On the editor, write the code as bellow
+```
+MAILTO='YOUR_EMAIL_ADRESS' # or "" not to send e-mail
+* * */1 * * python your/absolute/path/to/thesis-progress-monitor/monitor.py
+```
+
+3. To check what you wrote, run the command
+```shell
+$ crontab -l
+```
---
+++ post.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+ 
+from twitter import Twitter, OAuth
+import os
+import CoreGraphics as cg
+
+access_token = "YOUR_ACCESS_TOKEN"
+access_token_secret = "YOUR_SECRET_ACCESS_TOKEN"
+api_key = "YOUR_API_KEY"
+api_secret = "YOUR_SECRET_API_KEY"
+
+# TODO: create a log file if it does not exist
+log_file_path = 'YOUR_LOG_PATH'
+pdf_file_path = 'YOUR_PDF_PATH'
+your_name = 'YOUR_NAME'
+your_thesis_type = 'YOUR_THESIS_TYPE'
+
+t = Twitter(auth = OAuth(access_token, access_token_secret, api_key, api_secret))
+
+# read saved progress
+file = open(logFilePath, 'r')
+old_page = file.read()
+print('old_page:')
+print(old_page)
+
+# get current page number
+number_of_pages = cg.CGPDFDocumentCreateWithProvider(cg.CGDataProviderCreateWithFilename(pdfFilePath)).getNumberOfPages()
+print('current_page:')
+print(number_of_pages)
+
+# calc delta page
+delta_page = number_of_pages - int(old_page)
+print('delta_page:')
+print(delta_page)
+
+# make text to post
+text0 = your_name + 'さんの\n'
+text1 = your_thesis_type + '論文の総ページ数は，' + str(number_of_pages) + 'ページです！\n'
+text2 = '昨日からの進捗は' + str(delta_page) + 'ページです'
+text = text0 + text1 + text2
+print(text)
+
+# post tweet
+statusUpdate = t.statuses.update(status=text)
+
+# data
+print(statusUpdate['user']['screen_name'])
+print(statusUpdate['user']['name'])
+print(statusUpdate['text'])
+
+# save progress
+file2 = open(log_file_path, 'w')
+current_page = number_of_pages
+file2.write(str(current_page))
